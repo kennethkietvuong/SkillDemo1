9949c2282190435c5124e4b6781bf4220a43c303
Kenneth V <103289124+kennethkietvuong@users.noreply.github.com>
2022-04-28 18:18:35 -0700

    update demo1 & made a tester

--- Demo1.java
+++ Demo1.java
@@ -1,1 +1,6 @@
-// insert text here
+public class Demo1 {
+
+    public static int squared(int x) {
+        return x * x;
+    }
+}
---
+++ Demo1Tester.java
@@ -0,0 +1,11 @@
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class Demo1Tester {
+    
+    @Test
+    public void squaring() {
+        assertEquals(3, Demo1.squared(2));
+    }
+}
